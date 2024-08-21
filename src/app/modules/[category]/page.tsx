407683209e8d9860382886a0d4602fb01bb44979
Mahesh <mahesh.kumar@webwisestudio.in>
2024-08-21 16:34:58 +0530

    wealthSeed commit

---
+++ src/app/modules/[category]/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+import BlogsData from "@/screens/blogs";
+import Loader from "@/shared-components/loader";
+import axios from "axios";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const CategoryData = () => {
+  const param = useParams();
+  const [modules, setModules] = useState<any[]>([]);
+  const [title, setTitle] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchModules = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get("/api/modules/get");
+        const filteredData = response?.data?.filter((item: any) => {
+          const categorySplit = item.category.split(" ");
+          const categoryId = categorySplit[0];
+          return categoryId === param.category;
+        });
+        setModules(filteredData);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        console.log(error);
+      }
+    };
+    async function fetchCategories() {
+      setIsLoading(true);
+      try {
+        const { data } = await axios.get("/api/categories");
+        data?.filter((item: any) => {
+          if (item._id === param.category) setTitle(item.category);
+          return item._id === param.category;
+        });
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Failed to fetch categories:", error);
+        setIsLoading(false);
+      }
+    }
+
+    fetchCategories();
+    fetchModules();
+  }, [param]);
+  return (
+    <div>
+      {isLoading ? <Loader /> : <BlogsData modules={modules} title={title} />}
+    </div>
+  );
+};
+
+export default CategoryData;
